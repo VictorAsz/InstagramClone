7560ebb40ca8a4d0e2bbc367aa12fce06efd8733
Victor Assunção <victor.a.schumann@gmail.com>
2023-12-16 23:44:55 -0300

    Começo do publish rell

--- resources/js/Components/CreatePostOverlay.vue
+++ resources/js/Components/CreatePostOverlay.vue
@@ -37,5 +37,18 @@
     <button class="absolute right-3 cursor-pointer" @click="$event => closeOverlay()">
         <Close :size="27" fillColor="#FFFFFF"/>
     </button>
+
+    <div class="max-w-6xl h-[calc(100%-100px)] mx-auto mt-10 bg-white rounded-xl">
+        <div class="flex items-center justify-between w-full rounded-t-xl p-3 border-b border-b-gray-300">
+            <ArrowLeft :size="30" fillColor="#000000" @click="$event =>closeOverlay()"/>
+                <div class="text-lg font-extrabold">New Reel</div>
+                <button class="text-lg text-blue-500 hover:text-gray-900 font-extrabold">
+                    Share
+
+                </button>
+                
+            
+        </div>
+    </div>
  </div>
 </template>
