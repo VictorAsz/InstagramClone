11495c57a7286c41a738e632da78b814cc355dac
Victor Assunção <victor.a.schumann@gmail.com>
2023-12-18 20:22:08 -0300

    adicionado mais imports na Home

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -1,7 +1,11 @@
 <script setup>
     import {ref, onMounted, toRefs} from 'vue'
-    import {Head, Link, router} from '@inertiajs/vue3'
+    import {Head, Link, router} from '@inertiajs/vue3';
     import MainLayout from '@/Layouts/MainLayout.vue';
+    import 'vue3-carousel/dist/carousel.css'
+    import { Carousel, Slide, Navigation } from 'vue3-carousel';
+
+    import DotsHorizontal from 'vue-vue-material-design-icons/DotsHorizontal.vue';
 </script>
 
 <template>
