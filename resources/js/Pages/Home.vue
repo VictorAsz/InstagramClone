8d021e6da48f9bcc69fdde3f196344df68b096b3
Victor Assunção <victor.a.schumann@gmail.com>
2023-12-19 20:56:58 -0300

    carrousel pronto

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -38,16 +38,32 @@
                 :transition="500"
                 snapAlign="start"
             >
-                <Slide v-for="slide in 10" :key="slide" class="h-60">
+                <Slide v-for="slide in 10" :key="slide">
                     <Link href="/" class="relative mx-auto text-center mt-4 px-2 cursor-pointer">
                         <div class="absolute z-[-1] -top-[5px] left-[4px] rounded-full rotate-45 w-[64px] h-[64px] contrast-[1.3]  bg-gradient-to-t from-yellow-300 to-purple-500 via-red-500">
-
+                            <div class="rounded-full ml-[3px] mt-[3px] w-[58px] h-[58px] bg-white" />
                         </div>
+                        <img class="rounded-full w-[56px] h-[56px] -mt-[1px]" src="https://picsum.photos/id/54/300/320">
+                        <div class="text-xs mt-2 w-[60px] truncate text-ellipsis overflow-hidden">NAME HERE</div>
                     </Link>
                 </Slide>
+                <template #addons>
+                    <Navigation/>
+                </template>
             </Carousel>
         </div>
     </MainLayout>
    
 </template>
 
+<style>
+    .carousel__prev,
+    .carousel__next,
+    .carousel__prev:hover,
+    .carousel__next:hover {
+        color: rgb(49, 49, 49);
+        background-color: rgb(255, 255, 255);
+        border-radius: 100%;
+        margin: 0 20px;
+    }
+</style>
