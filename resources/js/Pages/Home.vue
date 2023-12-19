e16e8354a17363bc7ed637f4a50949ca4b01ce95
Victor Assunção <victor.a.schumann@gmail.com>
2023-12-19 20:30:17 -0300

    criado função onMounted

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -6,10 +6,24 @@
     import { Carousel, Slide, Navigation } from 'vue3-carousel';
 
     import DotsHorizontal from 'vue-vue-material-design-icons/DotsHorizontal.vue';
+
+    let wWidth = ref(window.innerWidth)
+    let currentSlide = ref(0)
+    let currentPost = ref(null)
+    let openOverlay = ref(false)
+
+    const props = defineProps({ posts: Object, allUsers: Object})
+    const {posts, allUsers } = toRefs(props)
+
+    onMounted(() => {
+        window.addEventListener('resize', () => {
+            wWidth.value = window.innerWidth
+        })
+    })
 </script>
 
 <template>
-    <Head title="HOME" />
+    <Head title="Instagram" />
 
     <MainLayout>
         
