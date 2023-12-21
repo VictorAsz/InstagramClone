f9d132e83e7e69c4117cb090da42f06953d40614
Victor Assunção <victor.a.schumann@gmail.com>
2023-12-21 19:29:12 -0300

    Componente do overlay do contéudo

--- resources/js/Components/ContentOverlay.vue
+++ resources/js/Components/ContentOverlay.vue
@@ -1,5 +1,14 @@
 <script setup>
+    import { ref, toRefs } from 'vue';
 
+import Heart from 'vue-material-design-icons/Heart.vue';
+import Comment from 'vue-material-design-icons/Comment.vue';
+
+defineEmits(['selectedPost'])
+const props = defineProps(['postByUser']);
+const { postByUser } = toRefs(props);
+
+let isHover = ref(false)
 
 </script>
 
