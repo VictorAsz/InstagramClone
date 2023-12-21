88ca6ade13965d51527a34f8e447dc2607b0b6cb
Victor Assunção <victor.a.schumann@gmail.com>
2023-12-21 04:41:41 -0300

    inicio da User Page

---
+++ resources/js/Pages/User.vue
@@ -0,0 +1,28 @@
+<script setup>
+    import { reactive, toRefs } from 'vue'
+    import { Head, Link, router } from '@inertiajs/vue3';
+    import MainLayout from '@/Layouts/MainLayout.vue';
+
+    
+    import Cog from 'vue-material-design-icons/Cog.vue';
+    import Grid from 'vue-material-design-icons/Grid.vue';
+    import PlayBoxOutline from 'vue-material-design-icons/PlayBoxOutline.vue';
+    import BookmarkOutline from 'vue-material-design-icons/BookmarkOutline.vue';
+    import AccountBoxOutline from 'vue-material-design-icons/AccountBoxOutline.vue';    
+
+    let data = reactive({ post: null })
+    const form = reactive({ file: null })
+
+    const props = defineProps({ postsByUser: Object, user: Object })
+    const { postsByUser, user } = toRefs(props)
+
+</script>
+
+<template>
+    <Head title="Instagram" />
+
+    <MainLayout>
+ 
+    </MainLayout>
+   
+</template>
--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,8 @@ use Inertia\Inertia;
 Route::get('/', function () {
     return Inertia::render('Home');
 });
+
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
