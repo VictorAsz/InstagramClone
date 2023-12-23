c39e89d6cd9a0dbeb25f0f8c767234d1e4d1c0c2
Victor Assunção <victor.a.schumann@gmail.com>
2023-12-22 23:03:59 -0300

    Começo da ligação com banco de dados e adicionado imagem ao user

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -33,6 +33,7 @@ class RegisteredUserController extends Controller
     {
         $request->validate([
             'name' => 'required|string|max:255',
+            'file' => '/user-placeholder.png',
             'email' => 'required|string|lowercase|email|max:255|unique:'.User::class,
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
         ]);
