19f494bc9ef7af3bcb93198d237cab7f75e063c0
Victor Assunção <victor.a.schumann@gmail.com>
2023-12-26 23:32:07 -0300

    nome e data no comentário

--- resources/js/Components/ShowPostOverlay.vue
+++ resources/js/Components/ShowPostOverlay.vue
@@ -76,8 +76,13 @@ const textareaInput = (e) => {
                             <div class="flex items-center justify-between">
                                 <div class="flex items-center">
                                     <img class="rounded-full w-[38px] h-[38px]" src="https://picsum.photos/id/54/800/820">
+                                    <div class="ml-4 font-extrabold text-[15px]">
+                                    NAME
+                                    <span class="font-light text-gray-700 text-sm">DATE HERE</span>
 
                                 </div>
+                                </div>
+                                
                             </div>
                         </div>
 
