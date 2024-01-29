8b487901b4bad5a0a63328bc617e10241298e642
Victor Assunção <victor.a.schumann@gmail.com>
2024-01-29 15:46:26 -0300

    Home controller return post collection

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,9 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Inertia\Inertia;
 
 class HomeController extends Controller
 {
-    //
+    public function index(){
+        $posts = Post::orderBy('created_at', 'desc')->get();
+        return Inertia::render('Home', [
+            'posts' => new AllPostsCollection($posts),
+            'allUsers' => User::all()
+        ]);
+    }
 }
