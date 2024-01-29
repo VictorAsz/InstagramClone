a3590b3d655c9843098ace33a7b89e4d8c7e3006
Victor Assunção <victor.a.schumann@gmail.com>
2024-01-29 18:44:03 -0300

    Comment Store

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -7,59 +7,31 @@ use Illuminate\Http\Request;
 
 class CommentController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
-    {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
+  
     /**
      * Store a newly created resource in storage.
      */
     public function store(Request $request)
     {
-        //
-    }
+        $request->validate([
+            'post_id' => 'required',
+            'user_id' => 'required',
+            'comment' => 'required',
+        ]);
+        $comment = new Comment;
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(Comment $comment)
-    {
-        //
-    }
+        $comment->post_id = $request->input('post_id');
+        $comment->user_id = $request->input('user_id');
+        $comment->text = $request->input('comment');
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Comment $comment)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, Comment $comment)
-    {
-        //
+        $comment->save();
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Comment $comment)
+    public function destroy($id)
     {
-        //
+        
     }
 }
