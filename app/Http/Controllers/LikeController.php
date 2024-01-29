4e55d74f7fd2396e942b4d1b6ea4090ead4a82d5
Victor Assunção <victor.a.schumann@gmail.com>
2024-01-29 15:58:02 -0300

    Like controller already

--- app/Http/Controllers/LikeController.php
+++ app/Http/Controllers/LikeController.php
@@ -7,59 +7,28 @@ use Illuminate\Http\Request;
 
 class LikeController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
-    {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
+  
     /**
      * Store a newly created resource in storage.
      */
     public function store(Request $request)
     {
-        //
-    }
+        $request->validate([ 'post_id' => 'required' ]);
+        $like = new Like;
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(Like $like)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Like $like)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, Like $like)
-    {
-        //
+        $like->user_id = auth()->user()->id;
+        $like->post_id = $request->input('post_id');
+        $like->save();
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Like $like)
+    public function destroy($id)
     {
-        //
+        $like = Like::find($id);
+        if (count(collect($like)) > 0) {
+            $like->delete();
+        }
     }
 }
