55b29181514103f58e8de8b755192a4f10d926ae
Victor Assunção <victor.a.schumann@gmail.com>
2024-01-30 06:00:32 -0300

    Controlllers endpoints

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\CommentController;
+use App\Http\Controllers\LikeController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
@@ -26,8 +30,20 @@ Route::get('/user', function () {
 
  Route::middleware('auth')->group(function () {
 
+    Route::get('/', [HomeController::class,'index'])->name('home.index');
+
+    Route::get('/users/{id}', [UserController::class,'show'])->name('users.show');
+    Route::post('/users', [UserController::class,'update'])->name('users.update');
+   
+
     Route::post('/posts', [PostController::class, 'store'])->name('posts.store');
     Route::delete('/posts/{id}', [PostController::class , 'destroy'])->name('posts.destroy');
+
+    Route::post('/comments', [CommentController::class, 'store'])->name('comments.store');
+    Route::delete('/comments/{id}', [CommentController::class , 'destroy'])->name('comments.destroy');
+
+    Route::post('/likes', [LikeController::class, 'store'])->name('likes.store');
+    Route::delete('/likes/{id}', [LikeController::class , 'destroy'])->name('likes.destroy');
 
     
  });
